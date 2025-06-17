1d2570df30502b52c1a8989a757ba446c71858ca
Richard Maxwell <richard@rsmaxwell.co.uk>
2025-06-17 10:57:10 +0100

    Multiple minor updates

--- src/app/user/token/AccessTokenService.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-import { TokenService } from "./tokenService";
-
-@Injectable({ providedIn: 'root' })
-export class AccessTokenService extends TokenService {
-  constructor() {
-    super('accessToken');
-  }
-}
--- src/app/user/token/RefreshTokenService.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-import { TokenService } from "./tokenService";
-
-@Injectable({ providedIn: 'root' })
-export class RefreshTokenService extends TokenService {
-  constructor() {
-    super('refreshToken');
-  }
-}
